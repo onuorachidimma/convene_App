438490fea5f73f0d72ce9d443eabcb81c9165772
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-03-01 19:46:59 +0100

    add social handles to footer and style

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,20 +5,31 @@ const Footer = () =>{
     return(
         <>
             <div className={styles.footerContainer}>
-                <div>
-                    <Logo className={styles.footerContainer}/>
-                    <p className={styles.slogan}>Where Questions Unite and Ideas Ignite!</p>
-                </div>
-                <div className={styles.footerContact}>
-                    <p className={styles.hello}>Email: <a className={styles.myEmailResume} href="mailto:convene@support.org?subject=Subject%20Here&body=Message%20Here">convene@support.org</a></p>
-                    <p className={styles.hello}>Tel: <a className={styles.myEmailResume} href="tel:+234111222333">+234111222333</a></p>
+                <div className={styles.footerContactContainer}>
+                    <div>
+                        <Logo className={styles.footerContainer}/>
+                        <p className={styles.slogan}>Where Questions Unite and Ideas Ignite!</p>
+                    </div>
+                    <div className={styles.footerContact}>
+                        <p className={styles.hello}>Email: <a className={styles.myEmailResume} href="mailto:convene@support.org?subject=Subject%20Here&body=Message%20Here">convene@support.org</a></p>
+                        <p className={styles.hello}>Tel: <a className={styles.myEmailResume} href="tel:+234111222333">+234111222333</a></p>
+                    </div>
+
+                    <div className={styles.newsletter}>
+                        <p className={styles.subscribe}>Subscribe to our Newsletter</p>
+                        <form action="">
+                            <input type="Email" placeholder="Email" className={styles.input} /><button className={styles.button}>Send</button>
+                        </form>
+                    </div>
+
+                    
                 </div>
 
-                <div className={styles.newsletter}>
-                    <p className={styles.subscribe}>Subscribe to our Newsletter</p>
-                    <form action="">
-                        <input type="Email" placeholder="Email" className={styles.input} /><button className={styles.button}>Send</button>
-                    </form>
+                <div className={styles.socialMedia}>
+                    <a href="#" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                    <a href="#" target="_blank"><i class="fa-brands fa-square-x-twitter"></i></a>
+                    
                 </div>
                 
             </div>
