7c2a99610e5b154866d9db85d8b7808dd50023e6
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-03-04 01:13:29 +0100

    creaet and style the sign up page. Validate form

--- src/pages/login-signUp/signup.jsx
+++ src/pages/login-signUp/signup.jsx
@@ -1,11 +1,86 @@
 import styles from "./login-signUp.module.css"
+import { object, string, number, date, InferType } from 'yup';
+import { Formik } from 'formik'
+import Logo from "../../../src/logo2.jpeg"
+import {Routes, Route, Link} from 'react-router-dom'
+
+
+let userSchema = object({
+    email: string().email(),
+    username: string().required().min(8).max(10),
+    password: string().required(),
+    createdOn: date().default(() => new Date()),
+});
 
 const SignUp = () =>{
-    return (
+    return(
         <>
-            <p>I am SignUp</p>
+            
+            <Formik
+            validationSchema={userSchema} initialValues={{ username: '', password: '' }}
+            >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleSubmit,
+                isSubmitting,
+            }) => (
+                <form className={styles.form} onSubmit={handleSubmit}>
+
+                    <div className={styles.formLogo}>
+                        <img src={Logo} alt="" />
+                    </div>
+
+                    <p className={styles.login}>Sign Up</p>
+                    <div className={styles.inputContainer}>
+                        <input
+                            type="email"
+                            name="mail"
+                            onChange={handleChange}
+                            value={values.email}
+                            placeholder="Email"
+                        />
+                        <div className={styles.formErrors}>{errors.email}</div>
+                    </div>
+
+                    <div className={styles.inputContainer}>
+                        <input
+                            type="text"
+                            name="username"
+                            onChange={handleChange}
+                            value={values.username}
+                            placeholder="Username"
+                        />
+                        <div className={styles.formErrors}>{errors.username}</div>
+                    </div>
+                    
+                    <div className={styles.inputContainer}>
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            value={values.password}
+                            placeholder="Password"
+                        />
+                        <div className={styles.formErrors}>{errors.password}</div>
+                    </div>
+                    
+                    <button type="submit" disabled={isSubmitting} className={styles.btn}>
+                        Sign Up
+                    </button>
+
+                    <div className={styles.alreadyHaveAccount}>
+                        <p>Already have an account?</p>
+                        <Link to="/login" className={styles.link}>Login</Link>
+                    </div>
+                </form>
+            )}
+            </Formik>
         </>
     )
+    
 }
 
 export default SignUp
